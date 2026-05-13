5b334f8959eda426d93f0584ac62e30c11dd5503
ThiagoMarinho12 <thiago.silva@dcomp.ufs.br>
2026-05-12 22:05:03 -0300

    Update questao4.java

--- questao4.java
+++ questao4.java
@@ -1,1 +1,20 @@
+public Node find(Object item) {
+    Node atual = head;
 
+    while (atual != null) {
+        if (atual.item.equals(item)) {
+            return atual;
+        }
+        atual = atual.next;
+    }
+
+    return null;
+}
+
+public void insertAfter(Node x, Object item) {
+    if (x == null) return;
+
+    Node novo = new Node(item);
+    novo.next = x.next;
+    x.next = novo;
+}
