51c11ecba7144355915212a79f446c7939d50668
ThiagoMarinho12 <thiago.silva@dcomp.ufs.br>
2026-05-12 22:04:51 -0300

    Update questao3.java

--- questao3.java
+++ questao3.java
@@ -12,3 +12,26 @@ public class Node {
         this.next = next;
     }
 }
+
+public class ListaSimples {
+    private Node head;
+
+    public ListaSimples() {
+        this.head = null;
+    }
+
+    public void insertFirst(Object item) {
+        Node novo = new Node(item);
+        novo.next = head;
+        head = novo;
+    }
+
+    public void imprimir() {
+        Node atual = head;
+        while (atual != null) {
+            System.out.print(atual.item + " -> ");
+            atual = atual.next;
+        }
+        System.out.println("null");
+    }
+}
