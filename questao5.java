4d86848597a2656e436753ef75332f4862310679
ThiagoMarinho12 <thiago.silva@dcomp.ufs.br>
2026-05-12 22:05:15 -0300

    Update questao5.java

--- questao5.java
+++ questao5.java
@@ -1,1 +1,38 @@
+public class Josephus {
+    public static void main(String[] args) {
 
+        int N = 6;
+        int M = 3;
+
+        Node primeiro = new Node(1);
+        Node atual = primeiro;
+
+        for (int i = 2; i <= N; i++) {
+            Node novo = new Node(i);
+            atual.next = novo;
+            atual = novo;
+        }
+
+        atual.next = primeiro; // fecha o círculo
+
+        Node anterior = atual;
+        atual = primeiro;
+
+        System.out.println("Ordem de eliminacao:");
+
+        while (atual.next != atual) {
+
+            for (int i = 1; i < M; i++) {
+                anterior = atual;
+                atual = atual.next;
+            }
+
+            System.out.println(atual.item);
+
+            anterior.next = atual.next;
+            atual = anterior.next;
+        }
+
+        System.out.println("\nSobrevivente: " + atual.item);
+    }
+}
