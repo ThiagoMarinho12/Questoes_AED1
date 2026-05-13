edcaa1233ca617c9695549c5809618e3a286543d
ThiagoMarinho12 <thiago.silva@dcomp.ufs.br>
2026-05-12 22:03:49 -0300

    Implement remove method to delete element by index

--- questao2.java
+++ questao2.java
@@ -1,1 +1,10 @@
+public void remove(int indice) {
+    if (indice < 0 || indice >= tamanho)
+        throw new IndexOutOfBoundsException("Indice invalido: " + indice);
 
+    for (int i = indice; i < tamanho - 1; i++) {
+        dados[i] = dados[i + 1];
+    }
+
+    tamanho--;
+}
